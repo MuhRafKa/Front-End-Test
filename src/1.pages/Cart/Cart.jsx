ad468acc5615b12f1f938f00e3acb1d005b6cd13
MuhRafKa <rafikadarman@protonmail.com>
2019-09-06 02:49:41 -0700

    first ini

---
+++ src/1.pages/Cart/Cart.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
+import Axios from 'axios'
+import {connect} from 'react-redux'
+import {urlApi} from '../../3.helpers/database'
+import swal from 'sweetalert'
+
+class Cart extends Component {
+    state = {
+        cartData : []
+    }
+
+    componentWillReceiveProps(newProps){
+        this.getDataCart(newProps.id)
+    }
+
+    componentDidMount(){
+        this.getDataCart(this.props.id)
+    }
+
+    deleteCartItem = (id) => {
+        Axios.delete(urlApi + 'cart/' + id)
+        .then((res) => {
+            swal('Success', 'Item Deleted', 'success')
+            this.getDataCart(this.props.id)
+        })
+        .catch((err) => {
+            swal('Error', 'There is an error', 'error')
+        })
+    }
+
+    getDataCart = (id) => {
+        Axios.get(urlApi + 'cart?userId=' + id)
+        .then(res => {
+            console.log(res)
+            this.setState({cartData : res.data})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    renderCart = () => {
+        var jsx = this.state.cartData.map((val, idx) => {
+            
+            return (
+                <tr>
+                    <td>{val.productName}</td>
+                    <td>{val.price - (val.price * (val.discount/100))}</td>
+                    <td><div className="btn-group">
+                        <button type="button" className="btn btn-secondary" onClick={() => this.onBtnEditQty('min', idx)}>-</button>
+                        <button type="button" className="btn btn-secondary">{val.quantity}</button>
+                        <button type="button" className="btn btn-secondary" onClick={() => this.onBtnEditQty('add', idx)}>+</button>
+                    </div></td>
+                    <td>{(val.price - (val.price * (val.discount/100))) * val.quantity}</td>
+                    <td><input type="button" className="btn btn-danger btn-block" onClick={() => this.deleteCartItem(val.id)} value="Delete"/></td>
+                </tr>
+            )
+            
+        })
+
+        return jsx
+    }
+
+    onBtnEditQty = (action, idx) => {
+        let arrCart = this.state.cartData
+
+        if(action == 'min'){
+            if(arrCart[idx].quantity > 1){
+                arrCart[idx].quantity -= 1
+                Axios.put(urlApi + 'cart/' + arrCart[idx].id, arrCart[idx])
+                .then(res => this.getDataCart(this.props.id))
+                .catch(err => console.log(err))
+            }
+        }else if(action == 'add'){
+            arrCart[idx].quantity += 1
+            Axios.put(urlApi + 'cart/' + arrCart[idx].id, arrCart[idx])
+            .then(res => this.getDataCart(this.props.id))
+            .catch(err => console.log(err))
+        }
+    }
+
+    render(quantity) {
+        if (quantity==null){
+            return (
+            <tr>
+            <Alert color="danger">
+            Cart is empty
+            </Alert>
+            </tr>
+            )
+        }else{
+        return (
+            <div className="container">
+                <table className="table mt-3 text-center">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderCart()}
+                    </tbody>
+                </table>
+            </div>
+        );
+        }
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        id : state.user.id,
+        username : state.user.username
+    }
+}
+
+export default connect(mapStateToProps)(Cart)
